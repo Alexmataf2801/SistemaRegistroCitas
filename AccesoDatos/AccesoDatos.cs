fdd0a376383adbde75d47814f77e677347d3b460
Alexmataf2801 <alexmataf@hotmail.com>
2020-03-27 17:59:53 -0600

    PRIMER COMMIT CON EL PROYECTO

---
+++ AccesoDatos/AccesoDatos.cs
@@ -0,0 +1,45 @@
+﻿using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Core.Objects;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccesoDatos
+{
+    public class AccesoDatos
+    {
+        BDSistemaRegistroCitasEntities entities = new BDSistemaRegistroCitasEntities();
+
+
+        public bool InsertarUsuario(Usuario usuario) {
+            bool Correcto = false;
+            ObjectParameter IdUsuario;
+
+            try
+            {
+                IdUsuario = new ObjectParameter("IdUsuario", typeof(int));
+                var info = entities.paInsertarUsuario(usuario.Nombre,
+                                                      usuario.PrimerApellido,
+                                                      usuario.SegundoApellido,
+                                                      usuario.Identificacion,
+                                                      usuario.CorreoElectronico,
+                                                      usuario.Telefono,
+                                                      usuario.Genero,
+                                                      IdUsuario);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+           
+
+
+            return Correcto;
+        }
+
+    }
+}
---
+++ SistemaRegistroCitas/Controllers/CalendarioController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SistemaRegistroCitas.Controllers
+{
+    public class CalendarioController : Controller
+    {
+        // GET: Calendario
+        public ActionResult Calendario()
+        {
+            return View();
+        }
+
+        public JsonResult Prueba()
+        {
+            return Json("",JsonRequestBehavior.AllowGet);
+        }
+
+
+    }
+}
