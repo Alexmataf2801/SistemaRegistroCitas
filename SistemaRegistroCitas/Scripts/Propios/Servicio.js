78fc843862938cac4734086988e1a98c802070ae
JoseMata1 <62778473+JoseMata1@users.noreply.github.com>
2020-04-15 15:02:09 -0600

    Combo de servicio

--- SistemaRegistroCitas/Scripts/Propios/Servicio.js
+++ SistemaRegistroCitas/Scripts/Propios/Servicio.js
@@ -1,1 +1,32 @@
-﻿
+﻿function ObtenerServicios() {
+
+    $.ajax({
+        type: "GET",
+        dataType: "JSON",
+        url: "/Servicio/ObtenerServicios/",
+
+        success: function (InfoServicios) {
+
+            var clasificacion = $("#Servicios");
+
+            $(InfoServicios).each(function (i, v) {
+                clasificacion.append('<option value="' + v.Id + '">' + v.Nombre + '</option>');
+
+            });
+
+
+        },
+
+        error: function () {
+            console.log('error')
+        },
+
+    });
+
+}
+
+
+
+$(document).ready(function () {
+    ObtenerServicios();
+});
