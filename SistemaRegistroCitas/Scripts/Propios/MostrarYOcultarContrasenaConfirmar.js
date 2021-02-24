a9dbe3f4c73d11a33b35dbfcc286369fd67e0732
JoseMata1 <62778473+JoseMata1@users.noreply.github.com>
2021-02-24 16:28:36 -0600

    Se agregaron js para MostrarYOcultar las contraseñas

---
+++ SistemaRegistroCitas/Scripts/Propios/MostrarYOcultarContrasenaConfirmar.js
@@ -0,0 +1,12 @@
+﻿document.getElementById("OjoConfirmar").addEventListener("click", () => {
+    if (document.getElementById("txtConfirmaContrasena").type == "password") {
+        document.getElementById("txtConfirmaContrasena").type = "text";
+        document.getElementById("OjoConfirmar").classList.remove("fa-eye");
+        document.getElementById("OjoConfirmar").classList.add("fa-eye-slash");
+    } else {
+        document.getElementById("txtConfirmaContrasena").type = "password";
+        document.getElementById("OjoConfirmar").classList.remove("fa-eye-slash");
+        document.getElementById("OjoConfirmar").classList.add("fa-eye");
+    }
+
+});
